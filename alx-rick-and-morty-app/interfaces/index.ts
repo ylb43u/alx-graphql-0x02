11afe3874565e38c2af29af9e2e074bd2c2c4bba
ylb43u <mesterrachid2002@gmail.com>
2025-08-20 16:10:47 +0100

    Query the graphql endpoint

---
+++ alx-rick-and-morty-app/interfaces/index.ts
@@ -0,0 +1,17 @@
+interface InfoProps {
+    pages: number
+    next: number
+    prev: number
+    count: number
+}
+
+export interface EpisodeProps {
+    id: number
+    name: string
+    air_date: string
+    episode: string
+    info: InfoProps
+}
+
+// export type EpisodeCardProps = Pick<EpisodeProps, 'id' | 'name' | 'air_date' | "episode">
+export type EpisodeCardProps = Omit<EpisodeProps, "info">;
